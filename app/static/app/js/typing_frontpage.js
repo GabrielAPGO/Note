bdff971c96600c6e15b0890bce57d64079070279
Hugo Souza <hugo.souza0610@gmail.com>
2023-06-27 20:32:46 -0400

    Fiz a página inicial

---
+++ app/static/app/js/typing_frontpage.js
@@ -0,0 +1,27 @@
+new TypeIt("#type-animation", {
+    speed: 50,
+    deleteSpeed: 25,
+    loop: true
+})
+.type("Qualquer hora")
+.pause(500)
+.delete(13)
+.type("Qualquer lugar")
+.pause(500)
+.delete(14)
+.type("Simples de usar")
+.pause(500)
+.delete(15)
+.type("Lindo")
+.pause(500)
+.delete(5)
+.type("Fácil")
+.pause(500)
+.delete(5)
+.type("Prático")
+.pause(500)
+.delete(7)
+.type("Rápido")
+.pause(500)
+.delete(6)
+.go();
